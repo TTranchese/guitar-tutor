8724beb1e83163efe1691b8c11520fde64250039
Tommaso Tranchese <95087228+TTranchese@users.noreply.github.com>
2026-08-12 00:01:06 +0200

    fix: typo

--- App.tsx
+++ App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
-import ExerciseScreen from './src/screens/ExcerciseScreen';
+import ExerciseScreen from './src/screens/ExerciseScreen';
 
 export default function App() {
   return (
